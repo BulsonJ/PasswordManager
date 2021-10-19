6316f4bf6408f106eb316c1d834b9991e0f5284d
Jack Bulson <jacktbulson@gmail.com>
2021-10-19 12:16:59 +0100

    Decrypt String Method
    Limits number of ascii characters to punctuation and letters.
    
    To implement: checks to cut down on possibilities of letters in words

--- PasswordManager/PasswordSecurity.cpp
+++ PasswordManager/PasswordSecurity.cpp
@@ -41,6 +41,48 @@ vector<vector<int>> PasswordSecurity::decrypt_password_first_result(string passw
 	return words;
 }
 
+vector<vector<vector<int>>> PasswordSecurity::decrypt_string(string password) {
+	vector<vector<vector<int>>> possible_strings;
+	vector<vector<int>> ascii_values;
+	decrypt_password_recursive_string(password, 0, possible_strings, ascii_values);
+
+	// go through possible words here
+
+	// for each possible string
+	vector<string> strings;
+	for (auto it = possible_strings.begin(); it < possible_strings.end(); it++) {
+		if (it - possible_strings.begin() == 0) continue;
+		string current_string;
+		vector<int> last_word_pos;
+
+		// for each possible character space
+		for (auto string_it = (*it).begin(); string_it < (*it).end(); string_it++) {
+			if ((*string_it).size() == 1) {
+				current_string += (*string_it)[0];
+				if ((*string_it)[0] == 32) {
+					last_word_pos.emplace_back(string_it - (*it).begin());
+				}
+			}
+			else {
+				current_string += "?";
+			}
+			// for each possible character in that space
+			for (auto char_it = (*string_it).begin(); char_it < (*string_it).end(); char_it++) {
+
+			}
+
+		}
+		strings.emplace_back(current_string);
+	}
+
+
+	// -------------------------------
+	for (auto it = strings.begin(); it < strings.end(); it++) {
+		cout << *it << endl;
+	}
+	return possible_strings;
+}
+
 void PasswordSecurity::decrypt_password_recursive(string password, int offset, vector<vector<vector<int>>>& possible_words, vector<vector<int>>& current_word_possibility) {
 
 	if (password.size() == 0) {
@@ -91,6 +133,29 @@ void PasswordSecurity::decrypt_password_recursive_single(string password, int of
 	}	
 }
 
+void PasswordSecurity::decrypt_password_recursive_string(string password, int offset, vector<vector<vector<int>>>& possible_words, vector<vector<int>>& current_word_possibility) {
+
+	if (password.size() == 0) {
+		possible_words.emplace_back(current_word_possibility);
+		return;
+	}
+
+	string current_numbers;
+	int count = 0;
+	while (current_numbers.size() < 3 && !(current_numbers == password)) {
+		current_numbers += password[count];
+		if (std::stoi(current_numbers) == 0) return;
+
+		vector<int> ascii_values_matching = get_ascii_string_list_from_collatz(std::stoi(current_numbers), offset);
+		if (!(ascii_values_matching.size() == 0)) {
+			current_word_possibility.emplace_back(ascii_values_matching);
+			decrypt_password_recursive_string(password.substr(count + 1, password.size() - count), std::stoi(current_numbers), possible_words, current_word_possibility);
+			current_word_possibility.pop_back();
+		}
+		count++;
+	}
+}
+
 void PasswordSecurity::print_ascii_collatz_values(int offset) {
 	for (int i = 1; i < 256; i++) {
 		cout << "char value: " << i <<  " + " << offset << " :" << collatz(i + offset) << endl;
@@ -117,6 +182,19 @@ int PasswordSecurity::get_ascii_from_collatz(int collatzNum, int offset) {
 vector<int> PasswordSecurity::get_ascii_list_from_collatz(int collatzNum, int offset) {
 	vector<int> ascii_values;
 	for (int i = 1; i < 256; i++) {
+		if (collatzNum == collatz(i + offset)) {
+			ascii_values.emplace_back(i);
+		}
+	}
+	return ascii_values;
+}
+
+vector<int> PasswordSecurity::get_ascii_string_list_from_collatz(int collatzNum, int offset) {
+	vector<int> ascii_values;
+	for (int i = 32; i < 123; i++) {
+		if (i > 32 && i < 43){
+			continue;
+		}
 		if (collatzNum == collatz(i + offset)) {
 			ascii_values.emplace_back(i);
 		}
--- PasswordManager/PasswordSecurity.h
+++ PasswordManager/PasswordSecurity.h
@@ -6,14 +6,19 @@ class PasswordSecurity
 {
 public:
 	static string encrypt_string(const string s);
+
 	static vector<vector<vector<int>>> decrypt_password(string password);
 	static vector<vector<int>> decrypt_password_first_result(string password);
+	static vector<vector<vector<int>>> decrypt_string(string password);
 	static void decrypt_password_recursive(string rest_of_password,int offset, vector<vector<vector<int>>>& possible_words, vector<vector<int>>& current_word_possibility);
 	static void decrypt_password_recursive_single(string rest_of_password, int offset, vector<vector<int>>& possible_words, vector<int>& current_word_possibility);
+	static void decrypt_password_recursive_string(string rest_of_password, int offset, vector<vector<vector<int>>>& possible_words, vector<vector<int>>& current_word_possibility);
+
 	static void print_ascii_collatz_values(int offset);
 	static vector<pair<int, int>> get_ascii_collatz_values(int offset);
 	static int get_ascii_from_collatz(int collatz, int offset);
 	static vector<int> get_ascii_list_from_collatz(int collatz, int offset);
+	static vector<int> get_ascii_string_list_from_collatz(int collatz, int offset);
 protected:
 	static int collatz(const int input);
 	static int collatz(const int input, const int count);
